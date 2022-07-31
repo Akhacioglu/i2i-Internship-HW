f98b5f41c3b21657728a0d6ddcf22dc4653debae
Ahmet Kemal <61085135+Akhacioglu@users.noreply.github.com>
2022-07-31 21:42:13 +0300

    Add files via upload

---
+++ Go_Refactor_Prime-notPrime/IsPrime_refactoring.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	file, err := os.Open("Numbers.txt")
+
+	checkNilErr(err)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, err := strconv.Atoi(line)
+
+		checkNilErr(err)
+
+		checkPrime(num)
+	}
+
+}
+
+func checkPrime(num int) bool {
+	if num < 2 {
+		fmt.Printf("%d is not a Prime Number \n", num)
+		return false
+	}
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			fmt.Printf("%d is not a Prime Number \n", num)
+			return false
+		}
+	}
+	fmt.Printf("%d is a Prime Number \n", num)
+	return true
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
